7f9c8c1fc8d43e12db4ecad4f74ea712e7dfdc26
Panagiotis Messinezis <panagiotis@messinezis.com>
2019-11-27 17:32:05 +0200

    Adjustments create/store/index/show

--- public/js/app.js
+++ public/js/app.js
@@ -49376,12 +49376,79 @@ window.VueApp = new Vue({
     v3: null,
     v4: null,
     v5: null,
-    v6: null
+    v6: null,
+    chartaccount: null,
+    chartaccount1: null,
+    mainaccount: null,
+    mainaccount1: null,
+    subaccount: null,
+    subaccount1: null,
+    mainaccounts: [],
+    mainaccounts1: [],
+    subaccounts: [],
+    subaccounts1: [],
+    CreditDebit: null
   },
   computed: {
     total: function total() {
       t = Number(this.v1) + Number(this.v2) + Number(this.v3) + Number(this.v4) + Number(this.v5) + Number(this.v6);
       return t ? t : '';
+    }
+  },
+  watch: {
+    chartaccount: function chartaccount(chartid) {
+      var vm = this;
+      axios.get('/api/accountsOfChart?chart_id=' + chartid).then(function (result) {
+        vm.mainaccounts = result.data.accounts;
+
+        if (vm.mainaccounts && vm.mainaccounts.length == 1) {
+          vm.mainaccount = vm.mainaccounts[0].id;
+        } else {
+          vm.mainaccount = null;
+        }
+      });
+    },
+    chartaccount1: function chartaccount1(chartid) {
+      var vm = this;
+      axios.get('/api/accountsOfChart?chart_id=' + chartid).then(function (result) {
+        vm.mainaccounts1 = result.data.accounts;
+
+        if (vm.mainaccounts1) {
+          if (vm.mainaccounts1.length == 1) {
+            vm.mainaccount1 = vm.mainaccounts1[0].id;
+          }
+        } else {
+          vm.mainaccount1 = null;
+        }
+      });
+    },
+    mainaccount: function mainaccount(accountid) {
+      var vm = this;
+      axios.get('/api/subaccountsOfAccount?account_id=' + accountid).then(function (result) {
+        vm.subaccounts = result.data.subaccounts;
+
+        if (vm.subaccounts) {
+          if (vm.subaccounts.length == 1) {
+            vm.subaccount = vm.subaccounts[0].id;
+          }
+        } else {
+          vm.subaccount = null;
+        }
+      });
+    },
+    mainaccount1: function mainaccount1(accountid) {
+      var vm = this;
+      axios.get('/api/subaccountsOfAccount?account_id=' + accountid).then(function (result) {
+        vm.subaccounts1 = result.data.subaccounts;
+
+        if (vm.subaccounts1) {
+          if (vm.subaccounts1.length == 1) {
+            vm.subaccount1 = vm.subaccounts1[0].id;
+          }
+        } else {
+          vm.subaccount1 = null;
+        }
+      });
     }
   }
 });
